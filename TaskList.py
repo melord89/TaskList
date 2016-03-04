032ff19712db9f28de3ce55258d12e82f6aba791
Pawel Nogiec <pawel.nogiec@nokia.com>
2016-03-04 22:14:53 +0100

    moja wersja TaskListy

--- TaskList.py
+++ TaskList.py
@@ -2,22 +2,25 @@ class TaskList(object):
     def __init__(self):
         self.task_list = []
 
-
     def get_task_list(self):
         return self.task_list
 
-    def add_several_tasks(self, tasks):
-        self.task_list.append(tasks)
-        # self.task_list.update({id : task_name})
+    def add_tasks(self, *tasks):
+        self.task_list += [task for task in tasks]
 
     def remove_task(self, task_name):
-        self.task_list.remove(task_name)
+        [self.task_list.remove(task) for task in self.task_list if task.name == task_name]
 
+    def get_task_id(self, task_name):
+        try:
+            return [self.task_list[index].id for index, task in enumerate(self.task_list) if task.name == task_name][0]
+        except IndexError:
+            return None
 
 class Task(object):
-    def __init__(self, _id, tname):
+    def __init__(self, _id, name):
         self.id = _id
-        self.tname = tname
+        self.name = name
 
 
 
--- test/test_TaskList.py
+++ test/test_TaskList.py
@@ -1,17 +1,17 @@
 import pytest
 @pytest.fixture
 def TaskList():
-    from TaskList import TaskList
+    from TaskList.TaskList import TaskList
     return TaskList()
 
 @pytest.fixture
 def Task():
-    from TaskList import Task
+    from TaskList.TaskList import Task
     return Task
 
 def test_can_create_Task(Task):
     task = Task(1, 'task1')
-    assert (task.id, task.tname) == (1, 'task1')
+    assert (task.id, task.name) == (1, 'task1')
 
 
 def test_can_create_task_list(TaskList):
@@ -28,24 +28,21 @@ def test_can_return_task_list(TaskList):
 
 @pytest.fixture
 def tasklist_with_one_task(TaskList, Task):
-    # TaskList.add_new_task('task1')
-    TaskList.add_several_tasks(Task(1, 'task1'))
+    TaskList.add_tasks(Task(1, 'task1'))
     return TaskList
 
 
 @pytest.fixture
 def tasklist_with_several_tasks(TaskList, Task):
-    # TaskList.add_new_task('task1')
-    # TaskList.add_new_task('task2')
-    # TaskList.add_new_task('task3')
-    TaskList.add_several_tasks(Task(1, 'task1'),
-                          Task(2, 'task2'),
-                          Task(3, 'task3'))
+    TaskList.add_tasks(Task(1, 'task1'),
+                       Task(2, 'task2'),
+                       Task(3, 'task3'))
     return TaskList
 
-
 def test_can_add_new_task(tasklist_with_one_task, Task):
-    assert Task(1, 'task1') in tasklist_with_one_task.get_task_list()
+    list_len = len(tasklist_with_one_task.get_task_list())
+    tasklist_with_one_task.add_tasks(Task(1, 'task1'))
+    assert len(tasklist_with_one_task.get_task_list()) == list_len + 1
 
 
 def test_check_length_after_remove(tasklist_with_several_tasks):
@@ -58,6 +55,9 @@ def test_can_remove_task(tasklist_with_several_tasks):
     tasklist_with_several_tasks.remove_task('task2')
     assert 'task2' not in tasklist_with_several_tasks.get_task_list()
     assert 'task1', 'task3' in tasklist_with_several_tasks.get_task_list()
+    tasklist_with_several_tasks.remove_task('task7')
+    assert 'task7' not in tasklist_with_several_tasks.get_task_list()
 
 def test_can_check_id(tasklist_with_one_task):
-    assert tasklist_with_one_task.get_task_id('task1') == 0
+    assert tasklist_with_one_task.get_task_id('task1') == 1
+    assert tasklist_with_one_task.get_task_id('task5') is None
